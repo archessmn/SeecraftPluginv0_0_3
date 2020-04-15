e74d4ec246f0d49d7c941a9ed479f6481f8fed51
archessmn <63592337+archessmn@users.noreply.github.com>
2020-04-15 13:06:05 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.github.archessmn.SeecraftPluginv0_0_3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
