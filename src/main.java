1da5e04c93878ed4cd00bcbc921af75495fd875b
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 12:23:39 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
